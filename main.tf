e32b02d4baaf12e3da860717e79bbc765e5a1cc0
devopsexercise1979 <109491325+devopsexercise1979@users.noreply.github.com>
2024-04-20 11:00:35 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "ap-southeast-2" # Change this to your desired region
 }
 
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "example-org-28b01e"
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "qr-lambda-app"
+        }
+    }
+}
+
 # S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "qr-code-generator7321"
