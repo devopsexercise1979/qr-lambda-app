631bf82c4de28555415fe70d69dd2d6623141acf
devopsexercise1979 <109491325+devopsexercise1979@users.noreply.github.com>
2024-04-20 12:33:22 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
+variable "aws_access_key" {}
+variable "aws_secret_access" {}
+
 provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access
   region = "ap-southeast-2" # Change this to your desired region
 }
 
