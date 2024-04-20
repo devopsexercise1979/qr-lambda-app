ef51f8fe0cd407df74b91abd7bf3fd53ed731d65
devopsexercise1979 <109491325+devopsexercise1979@users.noreply.github.com>
2024-04-20 20:45:25 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,6 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-2" # Change this to your desired region
-  profile = "default"
 #  access_key = var.AWS_ACCESS_KEY_ID
 #  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
